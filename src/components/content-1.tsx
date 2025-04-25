da9d974adc0728e39d539e83b4d1289c7b1851c0
Chris Sommers <cdsommers@gmail.com>
2025-04-24 23:07:42 -0400

    add animation to text

--- src/components/content-1.tsx
+++ src/components/content-1.tsx
@@ -1,10 +1,21 @@
 import Image from 'next/image'
+import { TextEffect } from '../../components/motion-primitives/text-effect'
+
+
+
+
 
 export default function ContentSection() {
     return (
         <section className="py-16 md:py-32">
             <div className="mx-auto max-w-5xl space-y-8 px-6 md:space-y-16">
-                <h2 className="relative z-10 max-w-xl text-4xl font-medium lg:text-5xl">From Architect to Leader: Christopher Sommers' Expertise in Enterprise IT</h2>
+                <h2 className="relative z-10 max-w-xl text-4xl font-medium lg:text-5xl">
+                    
+                    <TextEffect per='char' preset='fade'>
+                    From Architect to Leader: Christopher Sommers' Expertise in Enterprise IT
+                    </TextEffect>
+
+                    </h2>
                 <div className="grid gap-6 sm:grid-cols-2 md:gap-12 lg:gap-24">
                     <div className="relative mb-6 sm:mb-0">
                         <div className="bg-linear-to-b aspect-76/59 relative rounded-2xl from-zinc-300 to-transparent p-px dark:from-zinc-700">
